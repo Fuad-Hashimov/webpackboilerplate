5615334567eab96bc166e31458b269819f5d6185
Fuad_Hashimov <fuadheshimzade@gmail.com>
2023-05-11 10:38:29 +0400

    InitialCommit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+// const BundleAnalyzerPlugin =
+//   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: "production",
+  entry: {
+    bundle: path.resolve(__dirname, "src/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: "file-loader",
+        options: {
+          outputPath: "images",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hello Webpack",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    // new BundleAnalyzerPlugin(),
+  ],
+};
